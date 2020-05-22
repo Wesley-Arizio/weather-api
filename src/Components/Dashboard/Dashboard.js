cf63fb69a771137902c639e2d628a1e8b23f4053
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-05-22 14:51:49 -0300

    weatherstack API integration completed

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,29 +1,84 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import access_key from  '../../config/api_key.json'
+
 import './styles.css'
 
 export default class Dashboard extends Component{
+    constructor(props){
+        super(props)
+        this.state = { 
+            city: '',
+            access_key: "",
+
+            name:'',
+            country: " ",
+            temperature: '',
+            weather: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+
+    handleChange(e){
+        this.setState({
+            city: e.target.value
+        });
+    }
+
+    handleSubmit(e){
+        const params = {
+            access_key: access_key.access_key,
+            query: this.state.city
+        }
+        e.preventDefault()
+
+            try{
+                axios.get("http://api.weatherstack.com/current", { params })
+                    .then(response => {
+                        this.setState({
+                            name: response.data.location.name,
+                            country: response.data.location.country,
+                            temperature: response.data.current.temperature,
+                            weather: response.data.current.weather_descriptions
+                        })
+                        console.log(response.data)
+                });
+            }catch(error){
+                    console.log(error)
+                }
+    }
     render(){
+        
         return(
             <section className="section__container">
-                <div className="section__container--input">
-                    <input type="text"   className="input--city" placeholder="Type a city name: " />
+                <form className="section__container--input" onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text"
+                        className="input--city" 
+                        placeholder="Type a city name: " 
+                        value={ this.state.city }
+                        onChange={this.handleChange}
+                        />
                     <input type="submit" className="input-submit" name="send" value="SEND" />
-                </div>
+                </form>
                 
                 <section className="section__container--display">
                     <div className="section__container--card">
                         <ul className="card__region">
-                            <li>New York</li>
-                            <li>United States of America</li>
+                            <li>{this.state.name} </li>
+                            <li>{this.state.country}</li>
                         </ul>
                         <div className="card__temperature">
                             <div className="temperature">
-                                <span>17 C</span>
-                                <span>19 C</span>
+                                <span>{this.state.temperature} </span>
+                                <span> {this.state.weather}  </span>
                             </div>
                         </div>
                         <div className="card-weather">
-                            <span>Sunny</span>
+                            <span> {this.state.weather}  </span>
                         </div>
                     </div>
                 </section>
